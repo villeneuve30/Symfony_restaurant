8f1e3110ef3bbaf064cb87cb4a27aa78348ebfeb
Villeneuve Gaetan <gaetan.villeneuve@etu.unilim.fr>
2019-11-29 13:36:55 +0100

    dishes_category

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Dish;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FrontController extends AbstractController
@@ -49,15 +51,17 @@ class FrontController extends AbstractController
 
     /**
      * @Route("/carte/{id}", name="front_dishes_category", methods={"GET"})
+     * @param $id
+     * @return Response
      */
-    public function dishesCategory()
+    public function dishesCategory($id)
     {
         $entityManager = $this->getDoctrine()->getManager();
-        $categories = $entityManager->getRepository(Category::class)->findAll();
+        $dishes = $entityManager->getRepository(Dish::class)->findBy(['category' => $id]);
 
-        return $this->render('front/dishes.html.twig', [
+        return $this->render('front/dishes_category.html.twig', [
             'controller_name' => 'FrontController',
-            'categories' => $categories
+            'dishes' => $dishes
         ]);
     }
 }
